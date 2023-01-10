d78f5c2415895dcac8d24fd58da5748ff71a2425
snakehead-break <muhkhadua@gmail.com>
2023-01-10 09:03:31 +0700

    first commit

---
+++ application/views/frontend/include/base_footer.php
@@ -0,0 +1,36 @@
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-7  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Tiket BUS</h4>
+								<p>
+								Telkom Travel adalah jasa pemesanan tiket bis secara online. Telah dipercaya lebih dari 1 juta pelanggan secara nasional. Arga Travel menawarkan pemesanan tiket bis melalui website, iOS dan aplikasi android untuk rute-rute utama berbagai kota di Indonesia.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4  col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h4 class="text-white">Contact Us</h4>
+								<p>
+									Telkom Travel
+								</p>
+								<p class="number">
+									+62 81 111 111 111
+								</p>
+								
+							</div>
+						</div>											
+					</div>
+					<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
+            			<p class="footer-text m-0">Copyright &copy;2022 Telkom Travel</p>
+					</div>
+				</div>
+			</footer>
+			<div class="preloader">
+			<div class="loading">
+				<img src="<?php echo base_url('assets/frontend/img/preloader.gif') ?>" width="80">
+				<p>Harap Tunggu</p>
+			</div>
+		</div>	
+			<!-- End footer Area -->
